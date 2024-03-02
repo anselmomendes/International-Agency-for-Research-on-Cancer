c99fecb9085d445bf0ff5297a935dffcd147d2a5
Anselmo <anselmo.oliveira@dataway.info>
2024-03-02 16:00:46 -0300

    Atualização dos arquivos do projeto

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ import pandas as pd
 import sqlite3
 import streamlit as st
 
-con = sqlite3.connect("tutorial.db")
+con = sqlite3.connect("data/tutorial.db")
 attributable = pd.read_sql('select * from attributable', con=con)
 preventable = pd.read_sql('select * from preventable', con=con)
 
+st.markdown('''### __CASE 1:__
 
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
+Case para Seleção de Especialista em dados - Análise da Fração de Casos de Câncer Atribuíveis ao Índice de Massa Corporal (IMC) em 2012 na América do Sul.)
 
-st.write(attributable)
+##### __Introdução:__
 
-st.write(preventable)
+Você foi contratado para liderar uma análise que visa entender a fração de casos de câncer em 2012 atribuíveis ao excesso de Índice de Massa Corporal (IMC) em países da América do Sul. O objetivo é comparar essa fração entre os diferentes países da região.
+
+##### __Contexto:__
+
+Utilizaremos dados para analisar a carga de câncer atribuível ao excesso de peso corporal em diferentes países. Os dados estão disponíveis para ambos os sexos e abrangem todos os tipos de câncer em várias regiões anatômicas. Disponível em: https://gco.iarc.fr/causes/obesity/tools-map
+
+### __Desafio:__
+
+##### __Coleta de Dados:__
+
+- Reúna os dados de câncer incidente em 2012 por país, separados por sexo e região anatômica.
+
+- Faça uma apresentação gráfica e análise detalhada dos dados.
+
+- Faça uma comparação entre o cenário fração de casos de câncer em 2012 atribuíveis ao excesso de Índice de Massa Corporal (IMC) em países da América do Sul com o cenário previnível.
+''')
