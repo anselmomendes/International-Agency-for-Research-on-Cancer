4f98cf751b21006552bc8395e758ee2a9ce74e7c
Anselmo <anselmo.oliveira@dataway.info>
2024-03-03 13:25:05 -0300

    Ajuste de cast

--- pages/Desafio 1.py
+++ pages/Desafio 1.py
@@ -10,9 +10,9 @@ preventable = pd.read_sql('select * from preventable', con=con)
 
 st.markdown('### Reúna os dados de câncer incidente em 2012 por país, separados por sexo e região anatômica.')
 
-attributable['Attributable fraction M'] = attributable['paf_obs_m'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x,2)*100) + '%')
-attributable['Attributable fraction F'] = attributable['paf_obs_f'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x,2)*100) + '%')
-attributable['Attributable fraction B'] = attributable['paf_obs_both'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x,2)*100) + '%')
+attributable['Attributable fraction M'] = attributable['paf_obs_m'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x*100,2)) + '%')
+attributable['Attributable fraction F'] = attributable['paf_obs_f'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x*100,2)) + '%')
+attributable['Attributable fraction B'] = attributable['paf_obs_both'].apply(lambda x: '0%' if pd.isnull(x) else str(round(x*100,2)) + '%')
 
 attributable = attributable[['country', 'continent', 'cancer', 'Attributable fraction M', 'Attributable fraction F', 'Attributable fraction B']]   
 
